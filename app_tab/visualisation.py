406511b3fe62e76814236899cc46d77424cfa043
fgizzarelli <francesco.gizzarelli@impact-initiatives.org>
2024-10-06 11:39:17 +0200

    add import, interactive visualisation and variables mapping

--- app_tab/visualisation.py
+++ app_tab/visualisation.py
@@ -1,49 +1,71 @@
 import streamlit as st
 from app_utils import plot_tools as pt
 
-
 # Params
 THRESHOLD_QUANTITATIVE_TYPE = 20
 
-
 def display():
-     sub_tab1, sub_tab2 = st.tabs(["Univariate", "Bivariate"])
-     predic_data = st.session_state.ETL_output['features_prediction_score']
-     variable_types = pt.classify_variable_types(predic_data, THRESHOLD_QUANTITATIVE_TYPE)
-     hue_var_list = ['anomaly_prediction']
+    
+    if "variable_mapping" not in st.session_state:
+        st.error("Failed to load variable mapping.")
+        return
+    #st.write(st.session_state.variable_mapping)
+    features_list = list(st.session_state.variable_mapping.keys())
+    features_labels = list(st.session_state.variable_mapping.values())
 
-     # Exclude the 'enum_id', 'anomaly_score' and anomaly_prediction columns
-     features_list = [col for col in predic_data.columns if col not in ['audit_id', 'enum_id', 'anomaly_prediction']]
-     with sub_tab1:
-          col1, col2 = st.columns([1, 5])
-          with col1:
-               variable = st.selectbox("Choose a variable:", features_list, index=None)
-               hue_univariate = st.selectbox("Choose a grouping variable:", hue_var_list, key="HUE_univariate", index=0)
-          with col2:
-               if variable:
-                    fig = pt.univariate_ploting(df=predic_data, X=variable, hue=hue_univariate, variable_types=variable_types)
-                    st.pyplot(fig)
-     with sub_tab2:
-                col1, col2 = st.columns([1, 5])
-                with col1:
-                    X = st.selectbox("Choose X-axis variable", features_list, key="X", index = None)
-                    Y = st.selectbox("Choose Y-axis variable", features_list, key="Y", index = None)
-                    hue_bivariate = st.selectbox("a grouping variable:", hue_var_list, key="HUE_bivariate", index=0)
-                with col2:
-                    if X and Y:
-                         if hue_bivariate:
-                              kernel_plot = pt.kernel_density_plot(df=predic_data, X=X, Y=Y, hue=hue_bivariate)
-                              catplot = pt.catplot_multicat(df=predic_data, X=X, Y=Y, hue=hue_bivariate, variable_types=variable_types)
-                              distplot = pt.displot(df=predic_data, X=X, Y=Y, hue=hue_bivariate)
-                         else:
-                              kernel_plot = pt.kernel_density_plot(df=predic_data, X=X, Y=Y, hue=None)
-                              catplot = pt.catplot_multicat(df=predic_data, X=X, Y=Y, hue=None, variable_types=variable_types)
-                              distplot = pt.displot(df=predic_data, X=X, Y=Y, hue=None)
-                         kernel_tab, catplot_tab, displot_tab = st.tabs(["Kernel Density Plot", "Catplot", "Distplot"])
-                         with kernel_tab: 
-                              st.pyplot(kernel_plot)
-                         with catplot_tab: 
-                              st.pyplot(catplot)
-                         with displot_tab: 
-                              st.pyplot(distplot)
+    sub_tab1, sub_tab2 = st.tabs(["Univariate", "Bivariate"])
+    predic_data = st.session_state.ETL_output['features_prediction_score']
+    variable_types = pt.classify_variable_types(predic_data, THRESHOLD_QUANTITATIVE_TYPE)
+    hue_var_list = ["", 'anomaly_prediction']
+    labels_hue = ["None", "Anomaly Prediction"]
+    hue_mapping = dict(zip(hue_var_list, labels_hue))
+    reverse_hue_mapping = {v: k for k, v in hue_mapping.items()}
+
+    # Exclude certain columns from features_list
+    filtered_features_list = [col for col in features_list if col not in ['audit_id', 'enum_id', 'anomaly_prediction']]
+    filtered_labels_list = [label for col, label in zip(features_list, features_labels) if col not in ['audit_id', 'enum_id', 'anomaly_prediction']]
+    
+    with sub_tab1:
+        col1, col2 = st.columns([1, 5])
+        with col1:
+            variable = st.selectbox("Choose a variable:", filtered_labels_list, index=None)
+            hue_univariate_label = st.selectbox("Choose a grouping variable:", labels_hue, key="HUE_univariate", index=0)
+            hue_univariate = reverse_hue_mapping[hue_univariate_label] 
+            if hue_univariate == "":
+                hue_univariate = None
+        with col2:
+            if variable:
+                variable_name = filtered_features_list[filtered_labels_list.index(variable)]
+                pt.univariate_plotting2(df=predic_data, X=variable_name, hue=hue_univariate, variable_types=variable_types, x_label=variable)
+                fig = pt.univariate_plotting(df=predic_data, X=variable_name, hue=hue_univariate, variable_types=variable_types, x_label=variable)
+                st.pyplot(fig)
+
+    with sub_tab2:
+        col1, col2 = st.columns([1, 5])
+        with col1:
+            X = st.selectbox("Choose X-axis variable", filtered_labels_list, key="X", index=None)
+            Y = st.selectbox("Choose Y-axis variable", filtered_labels_list, key="Y", index=None)
+            hue_bivariate_label = st.selectbox("Choose a grouping variable:", labels_hue, key="HUE_bivariate", index=0)
+            hue_bivariate = reverse_hue_mapping[hue_bivariate_label]
+            if hue_bivariate == "":
+                hue_bivariate = None
+        with col2:
+            if X and Y:
+                X_name = filtered_features_list[filtered_labels_list.index(X)]
+                Y_name = filtered_features_list[filtered_labels_list.index(Y)]
+                
+                kernel_plot = pt.kernel_density_plot(df=predic_data, X=X_name, Y=Y_name, hue=hue_bivariate, x_label=X, y_label=Y)
+                catplot = pt.catplot_multicat(df=predic_data, X=X_name, Y=Y_name, hue=hue_bivariate, variable_types=variable_types, x_label=X, y_label=Y)
+                distplot = pt.displot(df=predic_data, X=X_name, Y=Y_name, hue=hue_bivariate, x_label=X, y_label=Y)
+
+                kernel_tab, catplot_tab, displot_tab = st.tabs(["Kernel Density Plot", "Catplot", "Distplot"])
+                with kernel_tab:
+                    st.pyplot(kernel_plot)
+                with catplot_tab:
+                    st.pyplot(catplot)
+                with displot_tab:
+                    st.pyplot(distplot)
+
+
+
 
--- app_utils/plot_tools.py
+++ app_utils/plot_tools.py
@@ -5,6 +5,17 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import shap
 import streamlit.components.v1 as components
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import altair as alt
+
+
+def make_subheader(_str, font_style="monospace", font_size=18):
+    subheader = f"<p style='font-family: {font_style}; color: white; font-size: {font_size}px;'>{_str}</p>"
+    st.markdown(subheader, unsafe_allow_html=True)
+    
+
 
 def classify_variable_types(df, threshold=20):
     """
@@ -36,66 +47,171 @@ def freedman_diaconis_rule(data):
     bins = int((data.max() - data.min()) / bin_width)
     return bins
 
+def load_df_excel_explanation():
+    if 'variable_names_excel' not in st.session_state:
+        st.error("Excel file path not set in session state.")
+        
+    excel_file_path = st.session_state['variable_names_excel']
+    df = pd.read_excel(excel_file_path)
+    return df
+
+def load_variable_attribute(var_name, var_attribute):
+
+    df = load_df_excel_explanation()
+
+    mapping = dict(zip(df[var_name], df[var_attribute]))
+
+    if not mapping:
+        st.error("Failed to load variable mapping. \
+                 Please check your header contains {} and {} as \
+                 columns".format(var_name, var_attribute))
+        
+    return mapping
+
 # Univariate Analysis
 @st.fragment
-def univariate_ploting(df, X, hue, variable_types ) -> None:
-     fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
-     # assigning a graph to each ax
-     if variable_types[X] == 'discrete':
-          # Count plot
-          sns.boxplot(data = df, x = X, hue=hue,
-                      orient="h", ax=ax_box, legend=False)
-          sns.countplot(data=df, x=X, hue= hue, ax=ax_hist)
-          # Remove x axis name for the boxplot
-          ax_box.set(xlabel='')
+def univariate_plotting(df, X, hue, variable_types, x_label=None) -> None:
+    fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.15, .85)})
 
-     elif variable_types[X] == 'non-numeric':  # New case for non-numeric variables
-          # Count plot for non-numeric variables
-          sns.countplot(data=df, x=X, hue=hue, ax=ax_hist)
-          ax_hist.set_xticklabels(ax_hist.get_xticklabels(), rotation=90)
-          # Set title and remove boxplot since it doesn't make sense for non-numeric
-          ax_box.axis('off')  # Hide the boxplot
-     else:
-          # Histogram
-          sns.boxplot(data = df, x = X, hue=hue,
-                             orient="h", ax=ax_box, legend=False)
-          sns.histplot(data=df, x=X, hue= hue, 
-                        stat="density",  element="step", common_norm=False, 
-                        ax=ax_hist)
-          # Remove x axis name for the boxplot
-          ax_box.set(xlabel='')
-     return(fig)
+    if variable_types[X] == 'discrete':
+        sns.boxplot(data=df, x=X, hue=hue, orient="h", ax=ax_box, legend=False)
+        sns.countplot(data=df, x=X, hue=hue, ax=ax_hist)
+        ax_box.set(xlabel='')  # Remove x-axis name for the boxplot
+    elif variable_types[X] == 'non-numeric':
+        sns.countplot(data=df, x=X, hue=hue, ax=ax_hist)
+        ax_hist.set_xticklabels(ax_hist.get_xticklabels(), rotation=90)
+        ax_box.axis('off')  # Hide the boxplot
+    else:
+        sns.boxplot(data=df, x=X, hue=hue, orient="h", ax=ax_box, legend=False)
+        sns.histplot(data=df, x=X, hue=hue, stat="density", element="step", common_norm=False, ax=ax_hist)
 
-# Bivariate Analysis 
-@st.fragment
-def kernel_density_plot(df, X, Y, hue) -> None:
-    fig = sns.JointGrid(data=df, x=X, y=Y,  hue=hue)
-    fig.plot_joint(sns.kdeplot, fill=False, alpha=0.4, common_norm=True, warn_singular=False)
-    fig.plot_joint(sns.rugplot, height=-.02, clip_on=False, alpha=.5 )
-    fig.plot_marginals(sns.boxplot)
+    # Set the x and y labels
+    if x_label:
+        ax_hist.set_xlabel(x_label)  # Set the x-axis label to the provided label
+
+    ax_box.set_ylabel('Density')  # Set y-axis label for the boxplot
+    ax_hist.set_ylabel('Count')    # Set y-axis label for the histogram
+
     return fig
 
 @st.fragment
-def catplot_multicat(df, X, Y, hue, variable_types) -> None:
+def univariate_plotting2(df, X, hue, variable_types, x_label=None):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, row_heights=[0.2, 0.8])
+
+    if hue:
+        unique_categories = df[hue].unique()
+        colors = ["#FFA500", "#636EFA"]  
+    else:
+        unique_categories = [None]  
+        colors = ['#636EFA']  
+
+    if variable_types[X] == 'continuous':
+        bin_size = (df[X].max() - df[X].min())/20 
+        #freedman_diaconis_rule(df[X])
+        bin_start = df[X].min()
+        bin_end = df[X].max()
+    else:
+        bin_size = 2  
+        bin_start = df[X].min()-1
+        bin_end = df[X].max()+1
+        
+    bins = dict(
+                start=bin_start,  
+                end=bin_end,      
+                size=bin_size     
+            )  
+
+    for i, category in enumerate(unique_categories):
+        if hue:
+            subset = df[df[hue] == category]
+            color = colors[i % len(colors)]
+        else:
+            subset = df
+            color = colors[0]
+        
+        box_trace = go.Box(x=subset[X], name=str(category) if hue else "", 
+                           boxmean='sd', orientation='h', marker=dict(color=color),
+                           legendgroup=str(category) if hue else "All Data",
+                           showlegend=False
+                            )
+        fig.add_trace(box_trace, row=1, col=1)
+
+    for i, category in enumerate(unique_categories):
+        if hue:
+            subset = df[df[hue] == category]
+            color = colors[i % len(colors)]
+        else:
+            subset = df
+            color = colors[0]
+        
+        hist_trace = go.Histogram(x=subset[X], name=str(category) if hue else "", opacity=0.7, marker=dict(color=color),
+                                  histnorm='probability density' if variable_types[X] != 'discrete' else None,
+                                  legendgroup=str(category) if hue else "All Data", showlegend=True,  
+                                  xbins=bins)
+        fig.add_trace(hist_trace, row=2, col=1)
+
+    fig.update_layout(
+        height=800, width=600,   
+        barmode='group' if variable_types[X] == 'discrete' else 'overlay', 
+        title='Univariate Plot for {} grouped by Anomaly Prediction'.format(x_label) if hue else 'Univariate Plot for {}'.format(x_label),
+        title_x=0.25 if hue else 0.35, legend=dict(title=hue if hue else "All Data")
+    )
+
+    if x_label:
+        fig.update_xaxes(title_text=x_label, row=2, col=1)
+    else:
+        fig.update_xaxes(title_text=X, row=2, col=1)
+
+    fig.update_yaxes(title_text="Density", row=1, col=1)  
+    fig.update_yaxes(title_text="Count", row=2, col=1)
     if variable_types[X] == 'discrete':
-        fig = sns.catplot(data=df, x=X, y =Y, hue=hue, kind="bar",
-                          estimator='mean', errorbar=('ci', 95))
+        fig.update_xaxes(tickvals=df[X].unique(), ticktext=df[X].unique(), ticks='outside', tickwidth=3, row=2, col=1)
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# Bivariate Analysis 
+@st.fragment
+def kernel_density_plot(df, X, Y, hue, x_label=None, y_label=None) -> None:
+    fig = sns.JointGrid(data=df, x=X, y=Y, hue=hue)
+    fig.plot_joint(sns.kdeplot, fill=False, alpha=0.4, common_norm=True, warn_singular=False)
+    fig.plot_joint(sns.rugplot, height=-.02, clip_on=False, alpha=.5)
+    fig.plot_marginals(sns.boxplot)
+
+    # Set axis labels
+    if x_label:
+        fig.set_axis_labels(x_label, y_label)
+
+    return fig
+
+@st.fragment
+def catplot_multicat(df, X, Y, hue, variable_types, x_label=None, y_label=None) -> None:
+    if variable_types[X] == 'discrete':
+        fig = sns.catplot(data=df, x=X, y=Y, hue=hue, kind="bar", estimator='mean', errorbar=('ci', 95))
     else:
         num_bins = freedman_diaconis_rule(df[X])
         df['quantitative_var_binned'] = pd.cut(df[X], bins=num_bins)
-        fig = sns.catplot(data=df, x="quantitative_var_binned", y =Y, hue=hue, 
-                          kind="bar", estimator='mean', errorbar=('ci', 95))
-        fig.set(xlabel=X)
+        fig = sns.catplot(data=df, x="quantitative_var_binned", y=Y, hue=hue, kind="bar", estimator='mean', errorbar=('ci', 95))
+
+    if x_label:
+        fig.set(xlabel=x_label, ylabel=y_label)  # Set both x and y axis labels
+
     fig.tick_params(axis='x', rotation=90)
     return fig
 
 @st.fragment
-def displot(df, X, Y, hue) -> None:
-    fig = sns.displot(df, x=X, y=Y, col=hue, rug=True)   
+def displot(df, X, Y, hue, x_label=None, y_label=None) -> None:
+    fig = sns.displot(df, x=X, y=Y, col=hue, rug=True)
+
+    for ax in fig.axes.flatten():
+        if x_label:
+            ax.set_xlabel(x_label)
+        if y_label:
+            ax.set_ylabel(y_label)
+
     return fig
 
 # SHAP Plot
-
 def st_shap(plot, height=None):
      shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
      components.html(shap_html, height=height)
@@ -116,3 +232,59 @@ def id_survey_shap_bar_plot(survey_id_var, selected_survey, data, shap_values) -
      fig, ax = plt.subplots()
      shap.plots.bar(shap_values[shap_index_survey],  max_display=nb_features, show=False, ax=ax)
      st.pyplot(fig)
+
+
+def id_survey_shap_bar_plot_interactive(survey_id_var, selected_survey, data, shap_values) -> None:
+    
+    def display_top_negative_shap_values(selected_index, shap_values, features, ntop=5) -> None:
+        survey_shap_values = shap_values[selected_index].values  
+        shap_series = pd.Series(survey_shap_values, index=features.columns)
+
+        negative_shap_df = shap_series[shap_series < 0].nsmallest(ntop)
+        
+        #st.write(negative_shap_df)
+        st.subheader("Top Features Anomaly Detection", divider="gray")
+        st.write("The following variable contributed the most to flag the selected \
+                survey as an anomaly")
+
+        for feature, value in negative_shap_df.items():
+            feature_label = st.session_state.variable_mapping.get(feature, feature)
+            feature_comment = st.session_state.variable_meaning.get(feature, "No comment available.")
+            full_string = f"- **{feature_label}**: SHAP value = \
+                            **{value:.4f}**. {feature_comment}"
+            st.markdown(full_string)
+        st.markdown("<hr style='height:2px; background-color: gray; border: none;' />", 
+                    unsafe_allow_html=True)       
+         
+    #nb_features = shap_values.data.shape[1]
+    index_survey = data[data[survey_id_var] == selected_survey].index[0]
+    shap_index_survey = data.index.get_loc(index_survey)
+    
+    shap_values_data = shap_values[shap_index_survey].values
+    feature_names = data.drop(columns=[survey_id_var]).columns
+    
+    feature_label = [st.session_state.variable_mapping.get(feature, feature) for feature in feature_names]
+    
+    shap_df = pd.DataFrame({
+        'Feature': feature_label,
+        'SHAP Value': shap_values_data
+    }).sort_values(by='SHAP Value', ascending=True)
+
+    chart = alt.Chart(shap_df).mark_bar().encode(
+        y=alt.Y('Feature:N', sort=None, axis=alt.Axis(labelFontSize=14, labelAngle=0)),  
+        x='SHAP Value:Q',
+        tooltip=['Feature', 'SHAP Value']
+    ).properties(
+        title=f'SHAP Values for Survey ID: {selected_survey}',
+        width=1200,
+        height=800
+    ).configure_mark(
+        opacity=0.8,
+        color='skyblue'
+    )
+
+    display_top_negative_shap_values(shap_index_survey, shap_values, 
+                                     data.drop(columns=[survey_id_var]), ntop=5)
+    st.altair_chart(chart, use_container_width=True)
+
+
